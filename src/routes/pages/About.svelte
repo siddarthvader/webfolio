962c63ec066fa914836b71ed01b59293197fc671
siddharth <siddharth@sushidata.com>
2023-03-15 23:37:36 +0530

    about page more or less done

--- src/routes/pages/About.svelte
+++ src/routes/pages/About.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Timeline from "../components/Timeline.svelte";
+
   function timeSince(since: number) {
     // birthday is a date
     var ageDifMs = Date.now();
@@ -29,40 +31,42 @@
 
 <div class="p-6 h-[calc(100%-48px)] w-full bg-veryPaleRed ">
   <div class="p-4 text-4xl  z-10 page-title font-semibold">About Me</div>
-  <div class="p-6  flex flex-row">
-    <div class="w-[69%]">
-      <div class="p-2 text-zinc-700">
-        <div class="text-2xl font-semibold  text-brightRed">
-          Hi, I am Siddharth
+  <div class="flex flex-row">
+    <div class="p-6  flex flex-row w-[60%]">
+      <div class="w-[69%]">
+        <div class="p-2 text-zinc-700">
+          <div class="text-2xl font-semibold  text-brightRed">
+            Hi, I am Siddharth
+          </div>
+          <div class="text-zinc-600 text-4xl font-bold py-2">
+            I make things work. <span class="typewriter" />
+          </div>
+          <div class="pt-2">
+            Software Developer/Architect from India with {timeSince(2014)}
+            years of experience writing Applications for web/mobile primarily. I
+            have been working as a full time freelancer for
+            {timeSince(2017)} years now and I got a chance to work on various technologies
+            ranging from writing bare CSS to work on complex data driven applications.
+          </div>
         </div>
-        <div class="text-zinc-600 text-4xl font-bold py-2">
-          I make things work. <span class="typewriter" />
-        </div>
-        <div class="pt-2">
-          Software Developer/Architect from India with {timeSince(2014)}
-          years of experience writing Applications for web/mobile primarily. I have
-          written fair amount of SaaS, Single Page applications using various frameworks,
-          concurretnly maintaing the quality of code and keeping it clean, as much
-          as the situation allowed. I have been working as a full time freelancer
-          for
-          {timeSince(2017)} years now and I got a chance to work on various technologies
-          ranging from writing bare CSS to work on complex data driven applications.
-        </div>
+        <button
+          class="bg-brightRed rounded-full px-4 py-1 text-white hover:bg-white hover:text-brightRed border-2 border-brightRed transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+          >Download CV</button
+        >
       </div>
-      <button
-        class="bg-brightRed rounded-full px-4 py-1 text-white hover:bg-white hover:text-brightRed border-2 border-brightRed transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-        >Download CV</button
-      >
+      <!-- <div class="w-[31%] px-4 border-l-2 border-veryPaleRed">
+        {#each domains as domain}
+          <h1 class="py-1 font-semibold text-zinc-500">{domain.name}</h1>
+          <div class="h-3 relative max-w-xl rounded-full overflow-hidden">
+            <div class="w-full h-full bg-gray-200 absolute" />
+            <div class="h-full bg-brightRed absolute" style="width:10%" />
+          </div>
+        {/each}
+      </div> -->
     </div>
-    <!-- <div class="w-[31%] px-4 border-l-2 border-veryPaleRed">
-      {#each domains as domain}
-        <h1 class="py-1 font-semibold text-zinc-500">{domain.name}</h1>
-        <div class="h-3 relative max-w-xl rounded-full overflow-hidden">
-          <div class="w-full h-full bg-gray-200 absolute" />
-          <div class="h-full bg-brightRed absolute" style="width:10%" />
-        </div>
-      {/each}
-    </div> -->
+    <div class="p-4 w-[40%] overflow-y-scroll max-h-[100%]">
+      <Timeline />
+    </div>
   </div>
 </div>
 
