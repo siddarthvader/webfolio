6ec104f802581e6d6bdf3aa92f4c82ee04d1b34c
siddharth <siddharth@sushidata.com>
2023-03-22 19:20:43 +0530

    build pushed

---
+++ build/_app/immutable/chunks/1.9c91f127.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7f06d12a.js";export{t as component};
---
+++ build/_app/immutable/chunks/2.27debb71.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.d5c6c974.js";export{t as component};
